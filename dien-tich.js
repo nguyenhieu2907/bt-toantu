ed156240c77f3f941fb5035d37fd79895dc8247c
nguyenhieu2907 <med290796@gmail.com>
2019-05-28 11:00:49 +0700

    ok

---
+++ dien-tich.js
@@ -0,0 +1,15 @@
+var inputWidth;
+var inputHeight;
+
+inputWidth = prompt("Enter the width: ");
+inputHeight = prompt("Enter the height: ");
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+
+var area = width * height;
+document.write("enter width :" +width);
+document.write("<br/>");
+document.write("enter height: " +height);
+document.write("<br/>");
+document.write("area : " +area);
+A
